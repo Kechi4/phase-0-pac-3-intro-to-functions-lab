c3420b6939dec0a4b7306ff63a149333d982b184
Nkechi Ukome <n.ukome@yahoo.com>
2022-12-11 22:52:27 -0500

    commit tree c9702d46d6e81f58d2088b35646d537997e17ff3

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout (string) {
+    console.log(string.toUpperCase( ));
+  }
+  function logWhisper (string) {
+    console.log(string.toLowerCase( ));
+  }
+  function sayHiToHeadphonedRoommate(string) {const cantUnswer = "I can't hear you!";
+ if (string.toLowerCase(string) === string) 
+    return cantUnswer;
+  
+  const yesUnswer = "YES INDEED!"
+if (string.toUpperCase(string) === string) 
+    return yesUnswer;
+ const lovUnswer = "I would love to!"
+if ("Let's have dinner together!" === string)
+return lovUnswer; }
